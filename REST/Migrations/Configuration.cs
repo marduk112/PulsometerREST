35e17c78d09785f2bc7aaa77407ebfad324f7e1c
Szymon Wójcik <szymon2wojcik@gmail.com>
2015-04-16 13:42:30 +0200

    Initial REST5

---
+++ REST/Migrations/Configuration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using REST.Models;
+
+namespace REST.Migrations
+{
+    public class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+            context.Books.AddOrUpdate(x => x.Id,
+                new Book()
+                {
+                    Id = 1,
+                    Title = "Pride and Prejudice",
+                    Year = 1813,
+                    Price = 9.99M,
+                    Genre = "Comedy of manners"
+                },
+                new Book()
+                {
+                    Id = 2,
+                    Title = "Northanger Abbey",
+                    Year = 1817,
+                    Price = 12.95M,
+                    Genre = "Gothic parody"
+                },
+                new Book()
+                {
+                    Id = 3,
+                    Title = "David Copperfield",
+                    Year = 1850,
+                    Price = 15,
+                    Genre = "Bildungsroman"
+                },
+                new Book()
+                {
+                    Id = 4,
+                    Title = "Don Quixote",
+                    Year = 1617,
+                    Price = 8.95M,
+                    Genre = "Picaresque"
+                }
+            );
+        }
+    }
+}
--- REST/Models/IdentityModels.cs
+++ REST/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace REST.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<REST.Models.Book> Books { get; set; }
     }
 }
