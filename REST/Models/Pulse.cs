81301fb5c0dc7debe5de15684acbed29a3bef9ad
Szymon Wójcik <szymon2wojcik@gmail.com>
2015-04-23 19:36:09 +0200

    Added Pulse model table

---
+++ REST/Models/Pulse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.Provider;
+
+namespace REST.Models
+{
+    public class Pulse
+    {
+        public int Id { get; set; }
+        [Required]
+        public int PulseValue { get; set; }
+        [Required]
+        public DateTime DateCreated
+        {
+            get
+            {
+                return _dateCreated.HasValue
+                   ? _dateCreated.Value
+                   : DateTimeOffset.UtcNow.UtcDateTime;
+            }
+
+            set { _dateCreated = value; }
+        }
+        [Required, ForeignKey("ApplicationUser")]
+        public int UserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        private DateTime? _dateCreated;
+    }
+}
