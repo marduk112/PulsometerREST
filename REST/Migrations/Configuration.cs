35ec05d78f1f74bfe571c62c72addb0ca74b6560
endablju <simaosrz@gmail.com>
2015-04-28 17:01:26 +0200

    Change id and server

--- REST/Migrations/Configuration.cs
+++ REST/Migrations/Configuration.cs
@@ -1,18 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Data.Entity.Migrations;
-using System.Linq;
-using System.Web;
-using REST.Models;
-
-namespace REST.Migrations
+﻿namespace REST.Migrations
 {
-    public class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<REST.Models.ApplicationDbContext>
     {
-        protected override void Seed(ApplicationDbContext context)
+        public Configuration()
         {
             AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(REST.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+
         }
     }
 }
