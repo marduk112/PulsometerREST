39dccc6732bb0d450e2c09aa632836a3659d6fee
Szymon Wójcik <szymon2wojcik@gmail.com>
2015-06-24 19:39:44 +0200

    c

--- REST/Models/DateDTO.cs
+++ REST/Models/DateDTO.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Web;
 
 namespace REST.Models
@@ -11,5 +13,19 @@ namespace REST.Models
         /// Date of measurement
         /// </summary>
         public DateTime MeasurementDate { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class DateDTOComparer : IEqualityComparer<DateDTO>
+    {
+        public bool Equals(DateDTO x, DateDTO y)
+        {
+            return x.MeasurementDate.Equals(y.MeasurementDate);
+        }
+
+        public int GetHashCode(DateDTO obj)
+        {
+            return obj.MeasurementDate.GetHashCode();
+        }
     }
 }
