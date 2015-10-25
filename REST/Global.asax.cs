f4f7d9a9c7a5f01322e04be2ba421d80c40a706c
Szymon Wójcik <szymon2wojcik@gmail.com>
2015-10-25 14:06:24 +0100

    Remove xml formatter

--- REST/Global.asax.cs
+++ REST/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -19,6 +20,10 @@ namespace REST
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //remove xml formatter
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            formatters.Remove(formatters.XmlFormatter);
+
         }
     }
 }
